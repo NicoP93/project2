7f31380cfbdebae44301246d64afa34087664120
NicoP93 <nicopcrr@gmail.com>
2019-04-17 11:27:35 -0600

    removed some files, camel cased seeder

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,16 +12,6 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-<<<<<<< HEAD:routes/htmlRoutes.js
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
-=======
   // app.get("/cryptid-view/:id", function(req, res) {
   //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
   //     res.render("cryptid-view", {
@@ -30,16 +20,17 @@ module.exports = function(app) {
   //   });
   // });
 
-    // Load example page and pass in an example by id
-    app.get("/cryptid-view/:id", function(req, res) {
-      db.Cryptid.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-        res.render("example", {
-          example: dbExample
-        });
+  // Load example page and pass in an example by id
+  app.get("/cryptid-view/:id", function(req, res) {
+    db.Cryptid.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
       });
     });
+  });
 
->>>>>>> 9541e7e773a830ee9f39b469a0d2738ea7c8ac6a:routes/html-routes.js
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
