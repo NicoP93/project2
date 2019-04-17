5657901873ad0ee680d581a1ad202bad18ec26b1
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-04-17 12:07:06 -0500

    fixed api stuff

---
+++ routes/post-api-routes.js
@@ -0,0 +1,49 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  // Get all examples
+  // Get route for retrieving a single post
+  app.get("/api/posts/:id", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Author
+    db.Post.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  // POST route for saving a new post
+  app.post("/api/posts", function(req, res) {
+    db.Post.create(req.body).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/posts/:id", function(req, res) {
+    db.Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/posts", function(req, res) {
+    db.Post.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+};
